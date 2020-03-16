bdc60e6a1df50f81e2bd76c82d6b61bb1aa2e46a
Khaled Ahmed <khaled.ahmed@ibtikar.net.sa>
2020-03-16 16:30:10 +0200

    feature edit wish STORY #56558 Work Done : Fix design for bottomSheet

--- app/src/main/java/co/publist/features/home/HomeActivity.kt
+++ app/src/main/java/co/publist/features/home/HomeActivity.kt
@@ -1,12 +1,12 @@
 package co.publist.features.home
 
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import co.publist.R
